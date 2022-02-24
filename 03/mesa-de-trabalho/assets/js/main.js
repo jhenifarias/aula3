c8ff3e968d5e4773a3caa959eb62196ae3d20f05
JhenifferFarias <jhenifferfarias.dev@gmail.com>
2022-02-23 21:57:15 -0300

    Corrigindo jogada, html e adicionando comentários para o próximo jogador

--- 03/mesa-de-trabalho/assets/js/main.js
+++ 03/mesa-de-trabalho/assets/js/main.js
@@ -7,8 +7,21 @@ linha2.innerText = "O";
 var linha3 = document.getElementById("linha-3_coluna-3");
 linha3.innerText = "O";
 
-var linha4 = document.getElementById("linha-2_coluna-1");
+var linha4 = document.getElementById("linha-3_coluna-2");
 linha4.innerText = "X";
 
-var linha5 = documento.getElementById("linha-2_coluna-2");
-linha5.innerText = "O";
+var linha5 = document.getElementById("linha-2_coluna-1");
+linha5.innerText = "X";
+
+
+
+
+// Bases vazias para a jogada:
+
+// "linha-1_coluna-2"
+// "linha-1_coluna-3"
+// "linha-2_coluna-3"
+// "linha-3_coluna-1"
+
+// Apagar a linha base usada após incluir no código.
+
