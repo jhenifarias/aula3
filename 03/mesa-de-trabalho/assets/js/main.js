0047022e5d7649e6df613f42ebfb03994dbaa573
JhenifferFarias <jhenifferfarias.dev@gmail.com>
2022-02-23 21:47:53 -0300

    Corrigindo um erro e adicionando mais uma jogada - Jheniffer

--- 03/mesa-de-trabalho/assets/js/main.js
+++ 03/mesa-de-trabalho/assets/js/main.js
@@ -1,20 +1,14 @@
 var linha1 = document.getElementById("linha-1_coluna-1");
-
 linha1.innerText = "X";
 
-
 var linha2 = document.getElementById("linha-2_coluna-2");
-
-
 linha2.innerText = "O";
 
-
 var linha3 = document.getElementById("linha-3_coluna-3");
-
-
 linha3.innerText = "O";
 
-var linha4 = document.getElementById("linha-3_coluna-2");
+var linha4 = document.getElementById("linha-2_coluna-1");
+linha4.innerText = "X";
 
-
-linha3.innerText = "X";
+var linha5 = documento.getElementById("linha-2_coluna-2");
+linha5.innerText = "O";
